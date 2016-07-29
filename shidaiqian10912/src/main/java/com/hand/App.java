e4c38d5a561761d101eeac47d5d99c2690ae3b3c
10912 <daiqian.shi@hand-china.com>
2016-07-29 17:28:40 +0800

    修改

--- shidaiqian10912/src/main/java/com/hand/App.java
+++ shidaiqian10912/src/main/java/com/hand/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
     	System.out.println( "Hello World!" );
-        System.out.println( "我的名字叫石代乾，你的名字呢？" );
+        System.out.println( "我的名字叫石代乾，你的名字呢？哇偶点卡说那就看见你时间能带上卡达" );
     }
 }
