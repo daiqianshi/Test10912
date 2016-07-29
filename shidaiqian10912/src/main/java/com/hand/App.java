e47b65b801de7c716b021a349ae83ec91109fbc9
10912 <daiqian.shi@hand-china.com>
2016-07-29 14:13:34 +0800

    1
    33

--- shidaiqian10912/src/main/java/com/hand/App.java
+++ shidaiqian10912/src/main/java/com/hand/App.java
@@ -4,12 +4,20 @@ package com.hand;
  * Hello world!
  *
  */
+
+class App2{
+	public static int number = 20;
+	public static void print(){
+		System.out.println(App2.number);
+	}
+}
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         print();
+        App2.print();
     }
     public static void print(){
     	System.out.println("测试是否乱码");
