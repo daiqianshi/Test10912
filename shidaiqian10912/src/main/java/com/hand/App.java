29cc31cca21b900871300fb69bb845061cc6dfd4
10912 <daiqian.shi@hand-china.com>
2016-07-29 11:22:43 +0800

    www
    wwww

--- shidaiqian10912/src/main/java/com/hand/App.java
+++ shidaiqian10912/src/main/java/com/hand/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        print();
+    }
+    public static void print(){
+    	System.out.println("我是打印方法");
     }
 }
